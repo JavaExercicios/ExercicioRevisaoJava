7a2e335516a2e02398ee858ae95c086e8d08bbd4
elencabral1 <rm98790@fiap.com.br>
2024-08-31 19:42:31 -0300

    feat: add Servlet

---
+++ src/main/java/iasi/CP2/ServletInitializer.java
@@ -0,0 +1,11 @@
+package iasi.CP2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Cp2JavaApplication.class);
+    }
+}
